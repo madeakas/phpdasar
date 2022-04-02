870fb0106e59aa6bb21936e310a33423eef8421c
madeakas <66621940+madeakas@users.noreply.github.com>
2022-04-02 13:31:31 +0700

    Pertemuan3

---
+++ pertemuan03/latihan3.php
@@ -0,0 +1,29 @@
+<?php
+//pengkondisian atau percabangan
+
+//if
+$x = 10;
+if( $x < 20){
+    echo "benar";
+}
+
+//if else
+$x = 40;
+if( $x < 20){
+    echo "benar";
+} else{
+    echo "salah";
+}
+
+//else if
+$x = 20;
+if( $x < 20){
+    echo "benar";
+} else if($x == 20){
+    echo "binggo";
+}
+else{
+    echo "salah";
+}
+
+?>
